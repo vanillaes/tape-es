5d818c02c1e2d0d24edaa0052447bcfa3ba086fc
Evan Plaice <evanplaice@gmail.com>
2019-11-29 20:16:03 -0700

    Refactor
    Refactor all the things

---
+++ src/runners.js
@@ -0,0 +1,26 @@
+import { spawn } from 'child_process';
+import { eachLimit } from './util.js';
+
+export async function run (test, root) {
+  spawn('node', [test], {
+    cwd: root,
+    stdio: ['pipe', process.stdout, process.stderr]
+  }).on('close', msg => {
+    if (msg === 1) { console.error('\x1b[31m%s\x1b[0m %s', 'ERR', 'Test failed!'); }
+  }).on('error', err => {
+    console.error(err);
+  });
+}
+
+export async function runAll (tests, max, root) {
+  await eachLimit(tests, max, function (test) {
+    spawn('node', [test], {
+      cwd: root,
+      stdio: ['pipe', process.stdout, process.stderr]
+    }).on('close', msg => {
+      if (msg === 1) { process.exitCode = 1; }
+    }).on('error', err => {
+      console.error(err);
+    });
+  });
+}
---
+++ src/util.js
@@ -0,0 +1,30 @@
+import { stat, readFile } from 'fs';
+import { promisify } from 'util';
+import glob from 'glob';
+const fileStatAsync = promisify(stat);
+const readFileAsync = promisify(readFile);
+const globAsync = promisify(glob);
+
+export async function readPkg (path) {
+  if (!await fileStatAsync(path)) {
+    throw Error('ERR_CONFIG: package.json not found, is this a package?');
+  }
+
+  try {
+    return JSON.parse(await readFileAsync(path, 'utf-8'));
+  } catch {
+    throw Error('ERR_CONFIG: Failed to read package.json');
+  }
+}
+
+export async function match (pattern, ignore, root) {
+  return globAsync(pattern, { cwd: root, ignore });
+}
+
+export async function eachLimit (items, limit, fn) {
+  Promise.all([...Array(limit)].map(async () => {
+    while (items.length > 0) {
+      await fn(items.pop());
+    }
+  }));
+}
