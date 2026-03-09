461ff6fe86508bd7593332d922c4dbb506888031
Evan Plaice <evanplaice@gmail.com>
2026-03-09 15:34:37 -0600

    Upgrade 'chokidar'

--- bin/tape-watch-es.js
+++ bin/tape-watch-es.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-import cli from 'commander'
-import chokidar from 'chokidar'
 import { run } from '../src/runners.js'
 import { readPkg } from '../src/util/index.js'
+import { glob } from 'node:fs/promises'
+import cli from 'commander'
+import chokidar from 'chokidar'
 
 const DEFAULT_PATTERN = '**/*.spec.js'
-const DEFAULT_IGNORE = '**/node_modules/**'
+const DEFAULT_IGNORE = 'node_modules/'
 const DEFAULT_ROOT = process.cwd();
 
 (async () => {
@@ -21,11 +22,13 @@ const DEFAULT_ROOT = process.cwd();
   const ignore = cli.ignore ? cli.ignore : DEFAULT_IGNORE
   const root = cli.root ? cli.root : DEFAULT_ROOT
 
-  const watcher = chokidar.watch(pattern, {
+  const files = await Array.fromAsync(glob(pattern, { cwd: root, exclude: [ignore] }))
+  const watcher = chokidar.watch(files, {
     ignored: [ignore],
     persistent: true,
     ignoreInitial: true,
-    cwd: root
+    cwd: root,
+    depth: 99
   })
   watcher.on('all', (event, path, stat) => run(path, root))
 })().catch(e => {
